a6106ffee4d94c69da7dcd582887e7962d83d197
abhijit kalta <abhijitkalta@gmail.com>
2016-08-25 03:50:51 +0530

    webpack config modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+var webpack = require('webpack');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
+var precss       = require('precss');
+var autoprefixer = require('autoprefixer');
 
 var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
   template: __dirname + '/app/index.html',
@@ -7,13 +10,32 @@ var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
 });
 
 module.exports = {
-  entry: __dirname + '/app/index.js' ,
+  entry: [
+   'webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+   'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+    __dirname + '/app/index.js' ] ,
   module : {
     loaders: [
       {
         test: /\.js$/,
         exclude: /node_modules/ ,
-        loader: 'babel-loader'
+        loaders: ['react-hot','babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css']
+      }, {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'postcss', 'sass']
+      }, {
+        test: /\.woff$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[path][name].[ext]"
+      }, {
+        test: /\.woff2$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff2&name=[path][name].[ext]"
+      }, {
+        test: /\.(eot|ttf|svg|gif|png)$/,
+        loader: "file-loader"
       }
     ]
   },
@@ -21,5 +43,12 @@ module.exports = {
     filename: 'transformed.js',
     path: __dirname + '/build'
   },
-  plugins: [HTMLWebpackPluginConfig]
+  plugins: [ new webpack.HotModuleReplacementPlugin(), HTMLWebpackPluginConfig,
+             new webpack.ProvidePlugin({
+             $: "jquery",
+            jQuery: "jquery"
+         })],
+  postcss: function () {
+        return [precss, autoprefixer];
+  }
 };
